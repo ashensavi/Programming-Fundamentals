0c01877445fd059238ceb01139978a47e89f16d5
ashensavi <ashensavinda0@gmail.com>
2024-10-19 11:47:03 +0530

    cc

--- Final Coursework-REDO/fashionshopredo.java
+++ Final Coursework-REDO/fashionshopredo.java
@@ -1,5 +1,42 @@
+import java.util.*;
 class fashionshopredo {
+    public static Scanner input = new Scanner(System.in);
     public static void main(String args[]){
-        System.out.println("Test");
+        homePage();
+    }
+
+    public static void homePage(){
+
+        System.out.println("===================Fashion SHOP===========================");
+        System.out.println("\n [1] Place Order");
+        System.out.println("\n [2] Search Customer");
+        System.out.println("\n [3] Search Order");
+        System.out.println("\n [4] View Reports");
+        System.out.println("\n [5] Set Order Status");
+        System.out.println("\n [6] Delete Order");
+        System.out.print("Enter an option: ");
+        int option = input.nextInt();
+
+        switch (option) {
+            case 1:
+                placeOrder();
+                break;
+            case 2:
+                searchCustomer();
+            case 3:
+                searchOrder();
+                break;
+            case 4:
+                viewReports();
+                break;
+            case 5:
+                setOrderStatus();
+            case 6:
+                deleteOrder();
+                break;
+            default:
+                System.out.println("Invalid Input! Please Enter a valid option...");
+        }
+
     }
 }

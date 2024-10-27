71bd2206736c363df6d3e181f441ca119601f734
ashensavi <ashensavinda0@gmail.com>
2024-10-27 12:47:10 +0530

    dd

--- Final Coursework-REDO/fashionshopredo.java
+++ Final Coursework-REDO/fashionshopredo.java
@@ -1,14 +1,16 @@
 import java.util.*;
+
 class fashionshopredo {
-    //Creating the arrays
-    public static String[] orderIdArray = new String[0]; // using 0 as the number of elements becuase we dont know how many orders will be there
+    // Creating the arrays
+    public static String[] orderIdArray = new String[0]; // using 0 as the number of elements becuase we dont know how
+                                                         // many orders will be there
     public static String[] phoneNumberArray = new String[0];
     public static String[] sizeArray = new String[0];
     public static int[] qtyArray = new int[0];
     public static double[] AmountArray = new double[0];
     public static int[] statusArray = new int[0];
-    
-    //Constants
+
+    // Constants
     public static final double XS = 600;
     public static final double S = 800;
     public static final double M = 900;
@@ -21,40 +23,40 @@ class fashionshopredo {
     public static final int DELIVERED = 2;
 
     public static Scanner input = new Scanner(System.in);
-    public static void main(String args[]){
+
+    public static void main(String args[]) {
         homePage();
     }
-    
-    
-    public final static void clearConsole() {  
-        try { 
-              final String os = System.getProperty("os.name");  
-              if (os.contains("Windows")) { 
-                  new ProcessBuilder("cmd", "/c", 
-   "cls").inheritIO().start().waitFor(); 
-              } else { 
-                   System.out.print("\033[H\033[2J");  
-                   System.out.flush(); 
-                 } 
-          } catch (final Exception e) { 
-               e.printStackTrace(); 
-               // Handle any exceptions. 
-           } 
-      } 
 
-    //===========Generate Order Id====================
-    public static String generateId(){
-        int id1;
-        if (orderIdArray.length>0){
-            id1=Integer.parseInt(orderIdArray[orderIdArray.length-1].split("[#]")[1]);
-            id1++;
-        }else{
-            return "ODR#00001";
+    public final static void clearConsole() {
+        try {
+            final String os = System.getProperty("os.name");
+            if (os.contains("Windows")) {
+                new ProcessBuilder("cmd", "/c",
+                        "cls").inheritIO().start().waitFor();
+            } else {
+                System.out.print("\033[H\033[2J");
+                System.out.flush();
+            }
+        } catch (final Exception e) {
+            e.printStackTrace();
+            // Handle any exceptions.
         }
-        return String.format("ODR#%05d",id1);
     }
 
-    public static void homePage(){
+    // ===========Generate Order Id====================
+    public static String generateId() {
+        int id1;
+        if (orderIdArray.length > 0) {
+            id1 = Integer.parseInt(orderIdArray[orderIdArray.length - 1].split("[#]")[1]);
+            id1++;
+        } else {
+            return "ODR#00001";
+        }
+        return String.format("ODR#%05d", id1);
+    }
+
+    public static void homePage() {
 
         System.out.println("===================Fashion SHOP===========================");
         System.out.println("\n [1] Place Order");
@@ -90,30 +92,30 @@ class fashionshopredo {
 
     }
 
-    public static boolean  validatePhoneNumber(String phoneNumber){
-        if (phoneNumber.length()==10 && phoneNumber.charAt(0)=='0') {
+    public static boolean validatePhoneNumber(String phoneNumber) {
+        if (phoneNumber.length() == 10 && phoneNumber.charAt(0) == '0') {
             return true;
         }
         return false;
     }
 
-    public static boolean validateQty(int qty){
-        if(qty>0){
+    public static boolean validateQty(int qty) {
+        if (qty > 0) {
             return true;
         }
         return false;
     }
 
-    public static void extendArrays(){
-         //create temp arrays for size 1 more than orderIdArray length.
-        String[] tempOrderIdArray = new String[orderIdArray.length+1];
-        String[] tempPhoneNumberArray = new String[orderIdArray.length+1];
-        String[] tempSizeArray = new String[orderIdArray.length+1];
-        int[] tempQtyrArray = new int[orderIdArray.length+1];
-        double[] tempAmountArray = new double[orderIdArray.length+1];
-        int[] tempStatusArray = new int[orderIdArray.length+1];
-       
-        for(int i=0 ; i < orderIdArray.length; i++){
+    public static void extendArrays() {
+        // create temp arrays for size 1 more than orderIdArray length.
+        String[] tempOrderIdArray = new String[orderIdArray.length + 1];
+        String[] tempPhoneNumberArray = new String[orderIdArray.length + 1];
+        String[] tempSizeArray = new String[orderIdArray.length + 1];
+        int[] tempQtyrArray = new int[orderIdArray.length + 1];
+        double[] tempAmountArray = new double[orderIdArray.length + 1];
+        int[] tempStatusArray = new int[orderIdArray.length + 1];
+
+        for (int i = 0; i < orderIdArray.length; i++) {
             tempOrderIdArray[i] = orderIdArray[i];
             tempPhoneNumberArray[i] = phoneNumberArray[i];
             tempSizeArray[i] = sizeArray[i];
@@ -121,7 +123,7 @@ class fashionshopredo {
             tempAmountArray[i] = AmountArray[i];
             tempStatusArray[i] = statusArray[i];
         }
-        //Swapping the array reference
+        // Swapping the array reference
         orderIdArray = tempOrderIdArray;
         phoneNumberArray = tempPhoneNumberArray;
         sizeArray = tempSizeArray;
@@ -130,127 +132,140 @@ class fashionshopredo {
         statusArray = tempStatusArray;
     }
 
-    public static int searchOrderByOrderId(String enteredOrderId){
-        for(int i=0;i<orderIdArray.length;i++){
+    public static int searchOrderByOrderId(String enteredOrderId) {
+        for (int i = 0; i < orderIdArray.length; i++) {
             if (orderIdArray[i].equals(enteredOrderId)) {
                 return i;
             }
-          
+
         }
-        return -1; // if the index is not there 
+        return -1; // if the index is not there
     }
 
     public static void printOrderDetails(int index) {
-        System.out.println("\nPhone Number : "+phoneNumberArray[index]);
-        System.out.println("\nSize : "+sizeArray[index]);
-        System.out.println("\nQty : "+qtyArray[index]);
-        System.out.println("\nAmount : "+AmountArray[index]);
-        if (statusArray[index]==0) {
+        System.out.println("\nPhone Number : " + phoneNumberArray[index]);
+        System.out.println("\nSize : " + sizeArray[index]);
+        System.out.println("\nQty : " + qtyArray[index]);
+        System.out.println("\nAmount : " + AmountArray[index]);
+        if (statusArray[index] == 0) {
             System.out.println("\nStatus : Processing");
-        }else if(statusArray[index]==1){
+        } else if (statusArray[index] == 1) {
             System.out.println("\nStatus : Delivered");
-        }else{
+        } else {
             System.out.println("\nStatus : Delivered");
         }
     }
 
-    //=========PLACE ORDER=============
-    public static void placeOrder(){
-        L1:do {
+    public static int searchCustomerByPhoneNumber(String enteredPhoneNumber) {
+        for (int i = 0; i < phoneNumberArray.length; i++) {
+            if (phoneNumberArray[i].equals(enteredPhoneNumber)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void getCustomerDetails(String enteredPhoneNumber){
+        
+    }
+
+    // =========PLACE ORDER=============
+    public static void placeOrder() {
+        L1: do {
             System.out.println("=============Place Order================");
             String orderId = generateId();
-            System.out.println("\nEnter Order ID: "+orderId);
-           
+            System.out.println("\nEnter Order ID: " + orderId);
+
             String phoneNumber;
-           L2: do{
+            L2: do {
                 System.out.print("\nEnter Customer Phone Number: ");
                 phoneNumber = input.next();
                 boolean isValidPhoneNumber = validatePhoneNumber(phoneNumber);
 
-                if(isValidPhoneNumber){
+                if (isValidPhoneNumber) {
                     break L2;
                 }
                 System.out.println("Invalid Phone Number!...Try Again!");
                 System.out.print("Do you want to enter phone number again? (Y/N)");
                 char ch = input.next().charAt(0);
-                if(ch == 'y' || ch == 'Y'){
-                     // Move the cursor up five lines 
-                     System.out.print("\033[6A"); 
-                     // Clear the lines 
-                     System.out.print("\033[0J");
-                     continue L2;
-                }else if(ch == 'n' || ch == 'N'){
+                if (ch == 'y' || ch == 'Y') {
+                    // Move the cursor up five lines
+                    System.out.print("\033[6A");
+                    // Clear the lines
+                    System.out.print("\033[0J");
+                    continue L2;
+                } else if (ch == 'n' || ch == 'N') {
                     clearConsole();
                     homePage();
                 }
-            }while(true);
+            } while (true);
 
             System.out.print("\nEnter T-Shirt Size(XS/S/M/L/XL/XXL): ");
             String size = input.next();
 
             int qty;
-            L3:do{
+            L3: do {
                 System.out.print("\nEnter QTY : ");
                 qty = input.nextInt();
 
                 boolean isValidQty = validateQty(qty);
 
-                if(isValidQty){
+                if (isValidQty) {
                     break L3;
                 }
                 System.out.println("Invalid QTY!.Try Again...");
                 System.out.print("Do you want to enter qty again? (Y/N)");
                 char ch = input.next().charAt(0);
-                if(ch == 'y' || ch == 'Y'){
-                     // Move the cursor up five lines 
-                     System.out.print("\033[6A"); 
-                     // Clear the lines 
-                     System.out.print("\033[0J");
-                     continue L3;
-                }else if(ch == 'n' || ch == 'N'){
+                if (ch == 'y' || ch == 'Y') {
+                    // Move the cursor up five lines
+                    System.out.print("\033[6A");
+                    // Clear the lines
+                    System.out.print("\033[0J");
+                    continue L3;
+                } else if (ch == 'n' || ch == 'N') {
                     clearConsole();
                     homePage();
                 }
-            }while(true);
+            } while (true);
 
             double amount = 0;
-            switch(size.toUpperCase()){
-                case "XS" :
+            switch (size.toUpperCase()) {
+                case "XS":
                     amount = XS * qty;
-                break;
-                case "S" :
+                    break;
+                case "S":
                     amount = S * qty;
-                break;
-                case "M" :
+                    break;
+                case "M":
                     amount = M * qty;
-                break;
+                    break;
                 case "L":
                     amount = L * qty;
-                break;
+                    break;
                 case "XL":
                     amount = XL * qty;
-                break;
+                    break;
                 case "XXL":
                     amount = XXL * qty;
                     break;
             }
-            System.out.println("\nAmount : "+amount);
+            System.out.println("\nAmount : " + amount);
             System.out.print("\nDo you want to place this order? (Y/N)");
             char ch = input.next().charAt(0);
 
             if (Character.toUpperCase(ch) == 'Y') {
                 extendArrays();
-                //Adding values to arrays
-                orderIdArray[orderIdArray.length-1]=orderId;
-                phoneNumberArray[phoneNumberArray.length-1] = phoneNumber;
-                sizeArray[sizeArray.length-1] = size;
-                qtyArray[qtyArray.length-1] = qty;
-                AmountArray[AmountArray.length-1] = amount;
-                statusArray[statusArray.length-1] = PROCCESSING;
+                // Adding values to arrays
+                orderIdArray[orderIdArray.length - 1] = orderId;
+                phoneNumberArray[phoneNumberArray.length - 1] = phoneNumber;
+                sizeArray[sizeArray.length - 1] = size;
+                qtyArray[qtyArray.length - 1] = qty;
+                AmountArray[AmountArray.length - 1] = amount;
+                statusArray[statusArray.length - 1] = PROCCESSING;
 
                 System.out.println("\nOrder Placed Successfuly!");
 
-                //Checking whether the values inserted successfuly
+                // Checking whether the values inserted successfuly
                 System.out.println(Arrays.toString(orderIdArray));
                 System.out.println(Arrays.toString(phoneNumberArray));
                 System.out.println(Arrays.toString(sizeArray));
@@ -258,89 +273,88 @@ class fashionshopredo {
                 System.out.println(Arrays.toString(AmountArray));
                 System.out.println(Arrays.toString(statusArray));
 
-
             }
             System.out.print("\nDo you want to place another order? (Y/N)");
             char choice = input.next().charAt(0);
             clearConsole();
-            if(Character.toUpperCase(choice)=='Y'){
+            if (Character.toUpperCase(choice) == 'Y') {
                 continue L1;
-            }else if(Character.toUpperCase(choice) == 'N'){
+            } else if (Character.toUpperCase(choice) == 'N') {
                 homePage();
             }
 
         } while (true);
     }
 
-
-    public static void searchCustomer(){
-        L5:do{
+    public static void searchCustomer() {
+        L5: do {
             System.out.println("\n=====================Search Customer=========================");
             System.out.println("\nEnter Customer Phone Number : ");
             String enteredPhoneNumber = input.next();
-            boolean isValidPhoneNumber=validatePhoneNumber(enteredPhoneNumber);
+            boolean isValidPhoneNumber = validatePhoneNumber(enteredPhoneNumber);
 
-            if(isValidPhoneNumber){
-                break L5;
+            if (isValidPhoneNumber) {
+                int index = searchCustomerByPhoneNumber(enteredPhoneNumber);
+
+                if (index == -1) {
+                    System.out.println("Invalid Phone Number!...Try Again!");
+                    System.out.print("Do you want to enter phone number again? (Y/N)");
+                    char ch = input.next().charAt(0);
+                    if (ch == 'y' || ch == 'Y') {
+                        // Move the cursor up five lines
+                        System.out.print("\033[6A");
+                        // Clear the lines
+                        System.out.print("\033[0J");
+                        continue L5;
+                    } else if (ch == 'n' || ch == 'N') {
+                        clearConsole();
+                        homePage();
+                    }
+                }else{
+                    getCustomerDetails(enteredPhoneNumber);
+                }
             }
-            System.out.println("Invalid Phone Number!...Try Again!");
-            System.out.print("Do you want to enter phone number again? (Y/N)");
-            char ch = input.next().charAt(0);
-            if(ch == 'y' || ch == 'Y'){
-                 // Move the cursor up five lines 
-                 System.out.print("\033[6A"); 
-                 // Clear the lines 
-                 System.out.print("\033[0J");
-                 continue L5;
-            }else if(ch == 'n' || ch == 'N'){
-                clearConsole();
-                homePage();
-            }
-        }while(true);
+
+        } while (true);
     }
 
-
-    public static void searchOrder(){
-        L4:do{
+    public static void searchOrder() {
+        L4: do {
             System.out.println("\n=====================Search Order=========================");
             System.out.println("\nEnter Order ID : ");
             String enteredorderId = input.next();
 
             int index = searchOrderByOrderId(enteredorderId);
-            
-            if (index==-1) {
+
+            if (index == -1) {
                 System.out.println("\nInvalid Order Id...");
-                
-            }else{
-                printOrderDetails(index);//passing index through this method to print the relavant details
+
+            } else {
+                printOrderDetails(index);// passing index through this method to print the relavant details
             }
             System.out.print("\nDo you want to search another order? (Y/N)");
             char ch = input.next().charAt(0);
             clearConsole();
 
-            if(Character.toUpperCase(ch)=='Y'){
+            if (Character.toUpperCase(ch) == 'Y') {
                 continue L4;
-            }else if(Character.toUpperCase(ch)== 'N'){
+            } else if (Character.toUpperCase(ch) == 'N') {
                 homePage();
             }
 
-        }while(true);
+        } while (true);
     }
 
-
-    public static void viewReports(){
-
-    }
-
-
-    public static void setOrderStatus(){
+    public static void viewReports() {
 
     }
 
-
-    public static void deleteOrder(){
+    public static void setOrderStatus() {
 
     }
 
+    public static void deleteOrder() {
+
+    }
 
 }
